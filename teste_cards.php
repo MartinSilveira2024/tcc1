6db83e91421665e0c2f64fa541dbc6e8e7a1f46b
Martín Silveira de Almeida <martin.2022311000@aluno.iffar.edu.br>
2024-08-08 16:20:41 -0300

    fotos

--- teste_cards.php
+++ teste_cards.php
@@ -5,7 +5,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="stylesheet" href="css.css">
 
-      <title>Landscape responsive card - Bedimcode</title>
+      <title>Jogos</title>
    </head>
    <body>
       <div class="container">
@@ -20,17 +20,17 @@
              </article>
 
              <article class="article_card">
-                <img src="jhin.jpg" alt="" class="card_img">
+                <img src="fort.jpg" alt="" class="card_img">
                 <div class="card_data">
-                    <span class="card_title"> Verlei</span>
-                    <h2 class="description_card">Tedy</h2>
+                    <span class="card_title"> Fortnite</span>
+                    <h2 class="description_card">Epic games</h2>
                     <a href="" class="card_buttom">Entre</a>
                 </div>
              </article>
              <article class="article_card">
-                <img src="jhin.jpg" alt="" class="card_img">
+                <img src="red.jpg" alt="" class="card_img">
                 <div class="card_data">
-                    <span class="card_title">Red dead</span>
+                    <span class="card_title">Red Dead Redemption 2</span>
                     <h2 class="description_card">Rockstar games</h2>
                     <a href="" class="card_buttom">Entre</a>
                 </div>
