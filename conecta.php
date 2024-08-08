262b93e86831a1ab41853eee7813ff70cf555a4f
Martín Silveira de Almeida <martin.2022311000@aluno.iffar.edu.br>
2024-08-08 15:24:05 -0300

    dsa

--- conecta.php
+++ conecta.php
@@ -2,7 +2,7 @@
 $servername="localhost";
 $username="root";
 $password="";
-$db_name="tcc";
+$db_name="tcc1";
 
 $connect = mysqli_connect($servername, $username, $password, $db_name);
 
--- usuarios/nivel_acesso.php
+++ usuarios/nivel_acesso.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once 'conecta.php';
+include_once '../conecta.php';
 $email = $_POST["email"];
 $senha = $_POST["senha"];
 
