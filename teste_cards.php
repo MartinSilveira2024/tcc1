d3e1052e63882c99448a76e62753073264ef3849
Martín Silveira de Almeida <martin.2022311000@aluno.iffar.edu.br>
2024-08-08 15:33:25 -0300

    css

---
+++ teste_cards.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+   <html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+      <!--=============== CSS ===============-->
+      <link rel="stylesheet" href="css.css">
+
+      <title>Landscape responsive card - Bedimcode</title>
+   </head>
+   <body>
+      <div class="container">
+        <div class="container_card">
+             <article class="article_card">
+                <img src="jhin.jpg" alt="" class="card_img">
+                <div class="card_data">
+                    <span class="card_title">League of legends</span>
+                    <h2 class="title_card">Riot games</h2>
+                    <a href="" class="card_buttom">Entre</a>
+                </div>
+             </article>
+
+             <article class="article_card">
+                <img src="jhin.jpg" alt="" class="card_img">
+                <div class="card_data">
+                    <span class="card_title"> Verlei</span>
+                    <h2 class="title_card">Tedy</h2>
+                    <a href="" class="card_buttom">Entre</a>
+                </div>
+             </article>
+             <article class="article_card">
+                <img src="jhin.jpg" alt="" class="card_img">
+                <div class="card_data">
+                    <span class="card_title">Red dead</span>
+                    <h2 class="title_card">Rockstar games</h2>
+                    <a href="" class="card_buttom">Entre</a>
+                </div>
+             </article> 
+        </div>
+      </div>
+   </body>
+</html>
