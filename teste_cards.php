c14c4f0cc34cec65fb2d43b8499fdef04fa5e3f9
Martín Silveira de Almeida <martin.2022311000@aluno.iffar.edu.br>
2024-08-08 16:08:31 -0300

    css arrumado

--- teste_cards.php
+++ teste_cards.php
@@ -3,8 +3,6 @@
    <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-      <!--=============== CSS ===============-->
       <link rel="stylesheet" href="css.css">
 
       <title>Landscape responsive card - Bedimcode</title>
@@ -16,7 +14,7 @@
                 <img src="jhin.jpg" alt="" class="card_img">
                 <div class="card_data">
                     <span class="card_title">League of legends</span>
-                    <h2 class="title_card">Riot games</h2>
+                    <h2 class="description_card">Riot games</h2>
                     <a href="" class="card_buttom">Entre</a>
                 </div>
              </article>
@@ -25,7 +23,7 @@
                 <img src="jhin.jpg" alt="" class="card_img">
                 <div class="card_data">
                     <span class="card_title"> Verlei</span>
-                    <h2 class="title_card">Tedy</h2>
+                    <h2 class="description_card">Tedy</h2>
                     <a href="" class="card_buttom">Entre</a>
                 </div>
              </article>
@@ -33,7 +31,7 @@
                 <img src="jhin.jpg" alt="" class="card_img">
                 <div class="card_data">
                     <span class="card_title">Red dead</span>
-                    <h2 class="title_card">Rockstar games</h2>
+                    <h2 class="description_card">Rockstar games</h2>
                     <a href="" class="card_buttom">Entre</a>
                 </div>
              </article> 
