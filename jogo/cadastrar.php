4eaf5adf4a3514ad3a1baeb562c48920b33530ad
Martín Silveira de Almeida <martin.2022311000@aluno.iffar.edu.br>
2024-08-26 16:37:54 -0300

    dsa

--- jogo/cadastrar.php
+++ jogo/cadastrar.php
@@ -14,11 +14,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="cad.css">
     <title>Document</title>
 </head>
 <body>
 
     <form action="cadastro.php" method="post" enctype="multipart/form-data">
+    <h1 id="h1">Cadastre o jogo</h1>
+    <div id="form-container">
     Insira a imagem do jogo:<input type="file" name="arquivo"><br> <br>
 
     <?php
@@ -33,5 +36,6 @@ foreach ($infos as $info) {
     Empresa do jogo:<input type="text" name="sub"> <br>
     <input type="submit" value="Enviar">
     </form>
+    </div>
 </body>
 </html>
