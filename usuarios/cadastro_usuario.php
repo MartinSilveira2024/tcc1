702306ab0c55886fae9599a36b32647874b250aa
Martín Silveira de Almeida <martin.2022311000@aluno.iffar.edu.br>
2024-10-04 11:30:05 -0300

    dsa

--- usuarios/cadastro_usuario.php
+++ usuarios/cadastro_usuario.php
@@ -12,12 +12,13 @@ $db_name="tcc";
 
 $connect = mysqli_connect($servername, $username, $password, $db_name);
 
+if($senha != $senha2) {
+	echo "A senha não confere com o repetir senha"; die;
+}
+
 if($senha == $senha2) {
 $sql = "INSERT INTO usuarios(nome_user, email_user, senha_user, nivel_acesso) VALUES ('$nome', '$email', '$senha', 'usr')";
 $query_cadastros = mysqli_query($connect,$sql);
-}
-else {
-	echo "Senha incorreta";
 }
 if ($query_cadastros)
 
