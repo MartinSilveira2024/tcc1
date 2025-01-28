b8a940795a1f89a028a64c34bddfdcb581531679
Martín Silveira de Almeida <martin.2022311000@aluno.iffar.edu.br>
2025-01-28 20:01:39 -0300

    sa

--- usuarios/excluir_usuario.php
+++ usuarios/excluir_usuario.php
@@ -10,9 +10,10 @@ if ($result = mysqli_query($conexao, $sql) === TRUE) {
     if ($conexao->errno == 1451) {
         echo "Não é possível excluir, pois este usuário está relacionado a outros dados no sistema."; die;
     } else {
-        echo "Erro";
+        echo "Error";
     }
 }
+// $result = mysqli_query($conexao, $sql);
 if ($result) {
     header("Location: ../index.php");
 } else {
